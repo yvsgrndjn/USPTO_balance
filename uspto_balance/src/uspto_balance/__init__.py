f5a4a21072707af08ed155eb2d9ee4381bab2c54
yvsgrndjn <yves.grandjean@unibe.ch>
2023-11-13 10:30:31 +0100

    remove old path

--- uspto_balance/src/uspto_balance/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Framework for dataset reaction types equilibration"""
-__name__ = 'uspto_balance'
-__version__ = "0.0.1"
-__author__ = "Yves Grandjean"
-__email__ = "yves.grandjean@unibe.ch"
-
-from .dataset_equilibration.py import main 
-
